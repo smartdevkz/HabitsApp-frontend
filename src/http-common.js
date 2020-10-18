9525e1992e000d17e8b4f8df4f967fd9bade6e58
Yerlan Karakulov <smartdevkz@mail.ru>
2020-10-18 14:34:36 +0300

    Changed API url

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://habits-api.smartdev.kz",
   headers: { "Content-type": "application/json" },
 });
 
